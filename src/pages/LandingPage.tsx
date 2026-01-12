2d4849927fdb4e82ecf28159a5b03f048315de88
AtharvaF-E-D <dhabekaratharva111@gmail.com>
2026-01-12 12:18:24 +0530

    new chnages

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -414,16 +414,124 @@ export default function LandingPage() {
     return texts[currentLocale] || texts.en;
   };
   return (
-    <div
-      className="
-    h-[422px]
-    md:h-[100vh] bg-cover bg-center bg-no-repeat
-    bg-[url('/src/assets/userImages/landingImgMobile.png')]   // mobile default
-    md:bg-[url('/src/assets/userImages/home_pageImg.png')]       // desktop
-  "
-    >
-      <UserHeader showBackButton={false} />
-      <SignUpModal
+    <>
+        <div className="
+         md:h-[100vh] h-[50vh] bg-cover bg-center bg-no-repeat
+         bg-[url('/src/assets/userImages/home_pageImg.png')] 
+        ">
+          <UserHeader showBackButton={false} />
+          <div className="h-[calc(100%-64px)] flex flex-col justify-end items-center ">
+            <div className={`w-full flex justify-center ${isMobile ? 'bg-gradient-to-b from-[rgba(1,53,31,0)] to-[#01351F] to-98%' : ''}`}>
+               <button
+              onClick={() => setShowSignUpModal(true)}
+              className="bg-[#91C73D] w-[188px]
+              
+                h-[55px] rounded-xl md:text-[18px] text-[16px] cursor-pointer px-7
+            font-semibold  text-white hover:bg-[#7fb32d] transition-colors figtree"
+            >
+              {t("landing.signUpButton")}
+            </button>
+            </div>
+            {!isMobile && <>
+             <h1 className="md:text-[66px] leading-15 text-[40px] bg-gradient-to-b from-[rgba(1,53,31,0)] to-[#01351F] to-98%   w-full text-white  text-center rosting-script font-[400] px-4">
+                {(() => {
+                  const title = t("landing.mainTitle");
+                  const [firstWord, ...rest] = title.split(" ");
+
+                  return (
+                    <>
+                      <span className="text-[#9dc123]">{firstWord}</span>{" "}
+                      {rest.join(" ")}
+                    </>
+                  );
+                })()}
+              </h1>
+              <div className="bg-gradient-to-b from-[rgba(1,53,31,0)] to-[#01351F] to-0% w-full">
+                <h2 className="text-white md:text-[60px] text-[40px] bg-gradient-to-b from-[rgba(1,53,31,0)] to-[#01351F]  leading-[64px]  max-w-4xl tracking-[0%] text-center  md:mx-auto mx-8 pb-3 px-4 font-[800] plus-jakarta-sans">
+                  {t("landing.subtitle")}
+                </h2>
+              </div>
+            </>}
+          </div>
+        </div>
+         {!isMobile && 
+         <div className="bg-[#01351f]">
+          <p className="w-full text-[#FFFFFF] max-w-7xl text-center md:text-[18px] text-[13px] mx-auto py-4 px-4 plus-jakarta-sans font-[300] tracking-[-0.4px]">
+            <Trans
+              i18nKey="landing.description"
+              components={{
+                bold: <span className="font-semibold" />,
+                br: <span className="block mt-3" />
+              }}
+            />
+          </p>
+         </div>
+          
+         }
+         {isMobile &&
+        <div className="bg-[#01351f]">
+          <h1 className="text-[40px] bg-gradient-to-b from-[rgba(1,53,31,0)] to-[#01351F] to-98%   w-full text-white  text-center rosting-script font-[400] px-4">
+                {(() => {
+                  const title = t("landing.mainTitle");
+                  const [firstWord, ...rest] = title.split(" ");
+
+                  return (
+                    <>
+                      <span className="text-[#9dc123]">{firstWord}</span>{" "}
+                      {rest.join(" ")}
+                    </>
+                  );
+                })()}
+              </h1>
+              <div className="bg-gradient-to-b from-[rgba(1,53,31,0)] to-[#01351F] to-0% w-full">
+                <h2 className="text-white text-[40px]  leading-[50px]  max-w-4xl tracking-[0%] text-center  md:mx-auto mx-8 pb-3 px-4 font-[800] plus-jakarta-sans">
+                  {t("landing.subtitle")}
+                </h2>
+              </div>
+          <p className="text-[#FFFFFF] text-center text-[13px]   py-2 px-4 plus-jakarta-sans font-[300] tracking-[-0.4px]">
+            <Trans
+              i18nKey="landing.description"
+              components={{
+                bold: <span className="font-semibold" />,
+                br: <span className="block mt-3" />
+              }}
+            />
+          </p>
+        </div>
+      }
+      <ServiceCarousel />
+       <div className="bg-[#01351f] py-7 ">
+        <h1 className="md:text-[64px] text-[36px] md:leading-[66px] leading-[42px] text-white text-center max-w-[350px] md:max-w-[845px] mx-auto font-[800] plus-jakarta-sans">
+          {t("landing.transparentJourney")}
+        </h1>
+        <p className="text-white text-center md:text-[18px] text-[13px] mx-auto max-w-7xl py-4 px-4 plus-jakarta-sans font-[300] tracking-[-0.4px]">
+          <Trans
+            i18nKey="landing.futureDescription"
+            components={[
+              <span className="block mt-2" />, 
+              <a
+                href="https://www.boligmatch.dk"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="font-semibold underline hover:text-gray-300 transition cursor-pointer"
+              />
+            ]}
+          />
+        </p>
+
+      </div>
+      <div
+        className="flex items-center justify-center w-full h-[100vh] mx-auto md:p-14"
+        style={{
+          backgroundImage: `url(${isMobile ? steperBgMobile : stepperBg})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat",
+        }}
+      ></div>
+    <Footer />
+
+    <SignUpModal
         open={showSignUpModal}
         onClose={() => setShowSignUpModal(false)}
         onSignupSuccess={handleSignupSuccess}
@@ -453,9 +561,7 @@ export default function LandingPage() {
         showSignUp={false}
         enableAutoLoginAfterSignup={false}
       />
-
-      {/* Recommendation Modal */}
-      {showRecommendationModal && recommendationData && !showSignUpModal && !showLoginModal && (
+{showRecommendationModal && recommendationData && !showSignUpModal && !showLoginModal && (
         <div className="fixed inset-0 z-[9999] flex items-center justify-center px-4">
           <div className="absolute inset-0 bg-black/20" />
           <div className="relative z-[10000] w-full max-w-sm bg-white rounded-2xl shadow-2xl px-6 py-7 text-center">
@@ -489,162 +595,7 @@ export default function LandingPage() {
           </div>
         </div>
       )}
-
-      {/* <div className="absolute md:top-28 top-28 lg:left-93 left-0 flex max-w-6xl mx-auto md:px-12 px-8 justify-between mt-0 md:h-[calc(100vh)]">
-        <div>
-          <div className="md:w-[130px] w-[70px] flex justify-end">
-            <img
-              src={landingPageIcons}
-              alt=""
-              className="md:w-[80px] w-10 h-auto shadow-black shadow-2xl rounded-full"
-            />
-          </div>
-          <div className="mb-2">
-            <img
-              src={landingPageIcons2}
-              alt=""
-              className="md:w-[80px] w-10 h-auto shadow-black shadow-2xl rounded-full"
-            />
-          </div>
-          <div className="">
-            <img
-              src={landingPageIcons3}
-              alt=""
-              className="md:w-[80px] w-10 h-auto shadow-black shadow-2xl rounded-full"
-            />
-          </div>
-          <div className="md:w-[130px] w-[70px] flex justify-end">
-            <img
-              src={landingPageIcons4}
-              alt=""
-              className="md:w-[80px] w-10 h-auto shadow-black shadow-2xl rounded-full"
-            />
-          </div>
-        </div>
-      </div> */}
-
-      <div className="pt-46 bg-gradient-to-b from-[rgba(1,53,31,0)] to-[#01351F] to-10% md:pt-0">
-
-        <div style={{ top: isMobile ? "350px" : "", }} className="md:absolute md:bottom-0 md:h-[350px] h-[350px]  flex flex-col items-center justify-end bg-gradient-to-b from-[rgba(1,53,31,0)] to-[#01351F] w-full ">
-          {isMobile ?
-            <>
-              <div className="absolute z-10  bg-gradient-to-b from-[rgba(1,53,31,0)] to-[#01351F] to-100%  w-full items-center justify-center flex flex-col">
-                <button
-                  onClick={() => setShowSignUpModal(true)}
-                  className="bg-[#91C73D] w-[188px]
-                md:h-[55px] h-[48px] rounded-xl md:text-[18px] text-[16px] cursor-pointer px-7
-            font-semibold  text-white hover:bg-[#7fb32d] transition-colors figtree"
-                >
-                  {t("landing.signUpButton")}
-                </button>
-                <div className="bg-[#01351f]">
-                  <h1 className="md:text-[66px] leading-15 text-[40px] bg-gradient-to-b from-[rgba(1,53,31,0)] to-[#01351F] to-98%   w-full text-white  text-center rosting-script font-[400] px-4">
-                    {(() => {
-                      const title = t("landing.mainTitle");
-                      const [firstWord, ...rest] = title.split(" ");
-
-                      return (
-                        <>
-                          <span className="text-[#9dc123]">{firstWord}</span>{" "}
-                          {rest.join(" ")}
-                        </>
-                      );
-                    })()}
-                  </h1>
-                  <div className=" w-full ">
-                    <h2 className="text-white md:text-[50px] text-[36px] md:leading-[50px] leading-[38px] max-w-4xl tracking-[0%] text-center md:mx-auto mx-0 pb-3 px-1 font-[800] plus-jakarta-sans">
-                      {t("landing.subtitle")}
-                    </h2>
-                  </div>
-                  <p className="text-[#FFFFFF] text-center md:text-[18px] text-[13px] mx-auto max-w-7xl md:py-4 py-2 px-4 plus-jakarta-sans font-[300] tracking-[-0.4px]">
-                    <Trans
-                      i18nKey="landing.description"
-                      components={{
-                        bold: <span className="font-semibold" />,
-                        br: <span className="block mt-2" />
-                      }}
-                    />
-                  </p>
-
-                </div>
-              </div>
-
-            </>
-            : <> <button
-              onClick={() => setShowSignUpModal(true)}
-              className="bg-[#91C73D]  w-[188px]
-                h-[55px] rounded-xl md:text-[18px] text-[16px] cursor-pointer px-7
-            font-semibold  text-white hover:bg-[#7fb32d] transition-colors figtree"
-            >
-              {t("landing.signUpButton")}
-            </button>
-              <h1 className="md:text-[66px] leading-15 text-[40px] bg-gradient-to-b from-[rgba(1,53,31,0)] to-[#01351F] to-98%   w-full text-white  text-center rosting-script font-[400] px-4">
-                {(() => {
-                  const title = t("landing.mainTitle");
-                  const [firstWord, ...rest] = title.split(" ");
-
-                  return (
-                    <>
-                      <span className="text-[#9dc123]">{firstWord}</span>{" "}
-                      {rest.join(" ")}
-                    </>
-                  );
-                })()}
-              </h1>
-              <div className="bg-gradient-to-b from-[rgba(1,53,31,0)] to-[#01351F] to-0% w-full">
-                <h2 className="text-white md:text-[60px] text-[40px] bg-gradient-to-b from-[rgba(1,53,31,0)] to-[#01351F]  leading-[64px]  max-w-4xl tracking-[0%] text-center  md:mx-auto mx-8 pb-3 px-4 font-[800] plus-jakarta-sans">
-                  {t("landing.subtitle")}
-                </h2>
-              </div>
-            </>
-          }
-        </div>
-      </div>
-      {!isMobile &&
-        <div className="bg-[#01351f] ">
-          <p className="text-[#FFFFFF] text-center md:text-[18px] text-[13px] mx-auto max-w-7xl md:py-4 py-2 px-4 plus-jakarta-sans font-[300] tracking-[-0.4px]">
-            <Trans
-              i18nKey="landing.description"
-              components={{
-                bold: <span className="font-semibold" />,
-                br: <span className="block mt-3" />
-              }}
-            />
-          </p>
-        </div>
-      }
-      <ServiceCarousel />
-      <div className="bg-[#01351f] py-7 pb-20">
-        <h1 className="md:text-[64px] text-[36px] md:leading-[66px] leading-[42px] text-white text-center max-w-[845px] mx-auto font-[800] plus-jakarta-sans">
-          {t("landing.transparentJourney")}
-        </h1>
-        <p className="text-white text-center md:text-[18px] text-[13px] mx-auto max-w-7xl py-4 px-4 plus-jakarta-sans font-[300] tracking-[-0.4px]">
-          <Trans
-            i18nKey="landing.futureDescription"
-            components={[
-              <span className="block mt-2" />, 
-              <a
-                href="https://www.boligmatch.dk"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="font-semibold underline hover:text-gray-300 transition cursor-pointer"
-              />
-            ]}
-          />
-        </p>
-
-      </div>
-      {/* <Stepper currentStep={currentStep} onStepClick={setCurrentStep} /> */}
-      <div
-        className="flex items-center justify-center w-full h-[100vh] mx-auto md:p-14"
-        style={{
-          backgroundImage: `url(${isMobile ? steperBgMobile : stepperBg})`,
-          backgroundSize: "cover",
-          backgroundPosition: "center",
-          backgroundRepeat: "no-repeat",
-        }}
-      ></div>
-      <Footer />
-    </div>
+    </>
+   
   );
 }
